b911b6a6f1d3554fbbd550eb874bc70b2b9271ae
pereznoe <arganopereznoe@gmail.com>
2024-04-25 15:18:41 +0530

    chore: renamed env vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const cherryPickedKeys = ["TMDB_API_READ_ACCESS_TOKEN", "TMDB_API_KEY"];
+const cherryPickedKeys = [
+  "REACT_APP_TMDB_API_READ_ACCESS_TOKEN",
+  "REACT_APP_TMDB_API_KEY",
+  "REACT_APP_OPENAI_API_KEY",
+];
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
